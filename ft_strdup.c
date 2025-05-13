d0f6732a6476b295ae22960a662ab958411467d4
Riyane El Qoqui <rel-qoqu@f1br6s1.clusters.42paris.fr>
2025-05-13 10:21:50 +0200

    chore push lib
    Signed-off-by: Riyane El Qoqui <rel-qoqu@f1br6s1.clusters.42paris.fr>

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 22:39:37 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/05/13 00:14:19 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s) + 1;
+	dup = (char *)malloc(sizeof(char) * (len));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
