c2d463439cd8ba54b90d6fa7525eac3964a95650
Purpose <riyane.dev@gmail.com>
2025-05-31 04:48:33 +0200

    fix(norm): update ft_max tabulation for norm

--- srcs/core/maths/ft_max.c
+++ srcs/core/maths/ft_max.c
@@ -6,13 +6,13 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 04:12:27 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/05/31 04:16:13 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/05/31 04:47:05 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core/ft_maths.h"
 
-int		ft_max(int a, int b)
+int	ft_max(int a, int b)
 {
 	if (a > b)
 		return (a);
