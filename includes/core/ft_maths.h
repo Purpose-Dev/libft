75085d5fc7f9f36f79431c65284962549ed6ed64
Purpose <riyane.dev@gmail.com>
2025-05-31 04:33:27 +0200

    feat(core): add additional mathematical utility functions to ft_maths.h

--- includes/core/ft_maths.h
+++ includes/core/ft_maths.h
@@ -16,20 +16,53 @@
 # define MIN(a, b) ft_min(a, b)
 # define MAX(a, b) ft_max(a, b)
 
-# define MAX(a, b) ({ \
-	typeof(a) _a = (a); \
-	typeof(b) _b = (b); \
-	if (_a > _b) \
-		_a; \
-	else \
-		_b; \
-})
-
+int		ft_mod(int a, int b);
 int		ft_abs(int n);
 long	ft_labs(long n);
 int		ft_min(int a, int b);
 int		ft_max(int a, int b);
 double	ft_pow(double base, int exp);
 double	ft_sqrt(double x);
+
+int		ft_gcd(int a, int b);
+int		ft_lcm(int a, int b);
+
+long	ft_factorial(int n);
+
+int		ft_clamp(int value, int min, int max);
+double	ft_clampf(double value, double min, double max);
+
+void	ft_swap(int *a, int *b);
+void	ft_swapf(double *a, double *b);
+
+int		ft_sign(int n);
+int		ft_signf(double n);
+
+double	ft_floor(double x);
+double	ft_ceil(double x);
+double	ft_round(double x);
+
+double	ft_trunc(double x);
+
+double	ft_deg_to_rad(double degrees);
+double	ft_rad_to_deg(double radians);
+
+double	ft_sin(double x);
+double	ft_cos(double x);
+double	ft_tan(double x);
+
+double	ft_sinh(double x);
+double	ft_cosh(double x);
+
+int		ft_double_equals(double a, double b, double epsilon);
+int		ft_is_zero(double x);
+int		ft_is_nan(double x);
+int		ft_is_inf(double x);
+
+int		ft_rand_range(int min, int max);
+double	ft_rand_double(void);
+long	ft_fibonacci(int n);
+int		ft_is_prime(int n);
+int		ft_next_prime(int n);
 
 #endif // FT_MATHS_H
