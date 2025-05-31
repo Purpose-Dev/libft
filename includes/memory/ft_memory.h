3ed52ad7f434c50b28723c341e812821f889b540
Purpose <riyane.dev@gmail.com>
2025-05-31 16:17:01 +0200

    feat(memory): add new memory utility functions to ft_memory.h

--- includes/memory/ft_memory.h
+++ includes/memory/ft_memory.h
@@ -24,6 +24,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memdup(const void *src, size_t n);
+void	*ft_memrchr(const void *s, int c, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+int		ft_memcasecmp(const void *s1, const void *s2, size_t n);
 
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_realloc(void *ptr, size_t size);
