8f21814f56ce469b909cfdeadba990355fae49b4
Purpose <riyane.dev@gmail.com>
2025-05-31 02:53:52 +0200

    refactor: add ft_realloc function prototype to ft_memory.h

--- includes/memory/ft_memory.h
+++ includes/memory/ft_memory.h
@@ -25,5 +25,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 void	*ft_calloc(size_t nmemb, size_t size);
+void    *ft_realloc(void *ptr, size_t size);
 
 #endif // FT_MEMORY_H
