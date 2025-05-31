342ab351044d3cac826b045e92745f117658f7be
Purpose <riyane.dev@gmail.com>
2025-05-31 04:06:56 +0200

    feat(core): enhance ft_maths.h with new functions and improved macros

--- includes/core/ft_maths.h
+++ includes/core/ft_maths.h
@@ -6,14 +6,36 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 03:01:44 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/05/31 03:02:43 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/05/31 04:06:43 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MATHS_H
 # define FT_MATHS_H
 
-# define MIN(a, b) ((a) < (b) ? (a) : (b))
-# define MAX(a, b) ((a) > (b) ? (a) : (b))
+# define MIN(a, b) ({ \
+	typeof(a) _a = (a); \
+	typeof(b) _b = (b); \
+	if (_a < _b) \
+		_a; \
+	else \
+		_b; \
+})
+
+# define MAX(a, b) ({ \
+	typeof(a) _a = (a); \
+	typeof(b) _b = (b); \
+	if (_a > _b) \
+		_a; \
+	else \
+		_b; \
+})
+
+int		ft_abs(int n);
+long	ft_labs(long n);
+int		ft_min(int a, int b);
+int		ft_max(int a, int b);
+double	ft_pow(double base, int exp);
+double	ft_sqrt(double x);
 
 #endif // FT_MATHS_H
