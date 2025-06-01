e0c4358617965542eaf44ed848c9d09cd48d3c68
Purpose <riyane.dev@gmail.com>
2025-06-01 11:05:51 +0200

    feat(random): add constants for random number generation

--- includes/core/utils/random/ft_random.h
+++ includes/core/utils/random/ft_random.h
@@ -6,14 +6,16 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 04:00:06 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/06/01 04:02:22 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/06/01 11:03:45 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_RANDOM_H
 # define FT_RANDOM_H
 
-# define FT_RAND_MAX 2147483647L // 2^31 - 1
+# define FT_RAND_MAX 2147483647L	// 2^31 - 1 (Max value for a random on 32-bit systems)
+# define FT_RAND_A 1103515245L		// Multiplier
+# define FT_RAND_C 12345L			// Increment
 
 void	ft_srand(unsigned int seed);
 int		ft_rand(void);
