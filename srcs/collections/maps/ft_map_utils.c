9ee75d6eee115907868767e35dae344529dd36ae
Purpose <riyane.dev@gmail.com>
2025-06-07 03:04:21 +0200

    feat(collections): add utility functions for map operations including string and numeric comparisons

---
+++ srcs/collections/maps/ft_map_utils.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 02:42:37 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/06/07 02:53:33 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "collections/maps/ft_map_utils.h"
+#include "core/ctype/ft_ctype.h"
+#include "core/memory/ft_memory.h"
+#include "core/string/ft_string.h"
+
+int	str_compare(const char *a, const char *b)
+{
+	if (!a && !b)
+		return (0);
+	if (!a)
+		return (-1);
+	if (!b)
+		return (1);
+	return (ft_strcmp(a, b));
+}
+
+int	str_compare_ignorecase(const char *a, const char *b)
+{
+	int	diff;
+
+	if (!a && !b)
+		return (0);
+	if (!a)
+		return (-1);
+	if (!b)
+		return (1);
+	while (*a && *b)
+	{
+		diff = ft_tolower((unsigned char)*a) - ft_tolower((unsigned char)*b);
+		if (diff != 0)
+			return (diff);
+		a++;
+		b++;
+	}
+	return (ft_tolower((unsigned char)*a) - ft_tolower((unsigned char)*b));
+}
+
+char	*key_duplicate(const char *key)
+{
+	size_t	len;
+	char	*dup;
+
+	if (!key)
+		return (NULL);
+	len = ft_strlen(key);
+	dup = malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	ft_memcpy(dup, key, len + 1);
+	return (dup);
+}
+
+void	key_free(char *key)
+{
+	if (key)
+		free(key);
+}
---
+++ srcs/collections/maps/ft_map_utils2.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map_utils2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 02:57:09 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/06/07 03:03:14 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "collections/maps/ft_map_utils.h"
+#include "core/ctype/ft_ctype.h"
+
+static void	count_digits(const char **str)
+{
+	while (ft_isdigit((unsigned char)**str))
+		(*str)++;
+}
+
+static int	compare_same_length_numbers(const char *a, const char *b, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		if (a[i] != b[i])
+			return ((unsigned char)a[i] - (unsigned char)b[i]);
+		i++;
+	}
+	return (0);
+}
+
+static int	compare_numbers(const char **a, const char **b)
+{
+	const char	*start_a;
+	const char	*start_b;
+	size_t		len_a;
+	size_t		len_b;
+
+	start_a = *a;
+	start_b = *b;
+	while (**a == '0' && ft_isdigit((unsigned char)*(*a + 1)))
+		(*a)++;
+	while (**b == '0' && ft_isdigit((unsigned char)*(*b + 1)))
+		(*b)++;
+	count_digits(a);
+	count_digits(b);
+	len_a = (size_t)(*a - start_a);
+	len_b = (size_t)(*b - start_b);
+	if (len_a != len_b)
+	{
+		if (len_a > len_b)
+			return (1);
+		return (-1);
+	}
+	return (compare_same_length_numbers(start_a, start_b, len_a));
+}
+
+static int	compare_numeric_part(const char **a, const char **b)
+{
+	if (ft_isdigit((unsigned char)**a) && ft_isdigit((unsigned char)**b))
+		return (compare_numbers(a, b));
+	if (**a != **b)
+		return ((unsigned char)**a - (unsigned char)**b);
+	(*a)++;
+	(*b)++;
+	return (0);
+}
+
+int	numeric_str_compare(const char *a, const char *b)
+{
+	int	result;
+
+	if (!a && !b)
+		return (0);
+	if (!a)
+		return (-1);
+	if (!b)
+		return (1);
+	while (*a && *b)
+	{
+		result = compare_numeric_part(&a, &b);
+		if (result != 0)
+			return (result);
+	}
+	return ((unsigned char)*a - (unsigned char)*b);
+}
