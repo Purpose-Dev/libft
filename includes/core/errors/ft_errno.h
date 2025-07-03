b073cc09f0b952a52e78f40b9027e69b225719d2
Riyane El Qoqui <rel-qoqu@student.42.fr>
2025-07-03 22:40:02 +0200

    feat(errno): add errno codes
    Signed-off-by: Riyane El Qoqui <rel-qoqu@student.42.fr>

---
+++ includes/core/errors/ft_errno.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_errno.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 19:42:17 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/07/03 19:44:33 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ERRNO
+# define FT_ERRNO
+
+# define ESNULLP 400  // null ptr
+# define ESZEROL 401  // length is zero
+# define ESLEMAX 403  // length exceeds RSIZE_MAX
+
+#endif // FT_ERRNO
