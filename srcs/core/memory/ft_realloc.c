fb06b81e6366f308c05599cfc24b5be539c42bde
Riyane El Qoqui <rel-qoqu@student.42.fr>
2025-07-03 19:17:14 +0200

    fix(realloc): fix internal size tracking method (causes invalid read)
    Signed-off-by: Riyane El Qoqui <rel-qoqu@student.42.fr>

--- srcs/core/memory/ft_realloc.c
+++ srcs/core/memory/ft_realloc.c
@@ -6,22 +6,12 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 02:10:18 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/05/31 23:05:20 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/07/03 18:49:51 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <malloc.h>
 #include "core/memory/ft_memory.h"
-
-static size_t	ptr_size(const void *ptr)
-{
-	const char	*c_ptr = ptr;
-	size_t		i;
-
-	i = 0;
-	while (*c_ptr++ && i < SIZE_MAX - 1)
-		i++;
-	return (i);
-}
 
 void	*ft_realloc(void *ptr, const size_t size)
 {
@@ -35,15 +25,12 @@ void	*ft_realloc(void *ptr, const size_t size)
 		free(ptr);
 		return (NULL);
 	}
-	if (size > SIZE_MAX)
-		return (NULL);
-	old_size = ptr_size(ptr);
-	if (old_size == size)
-		return (ptr);
+	old_size = malloc_usable_size(ptr);
+	if (old_size == size) return (ptr);
 	new_ptr = malloc(size);
 	if (!new_ptr)
 		return (NULL);
-	if (old_size < size)
+	if (old_size > size)
 		old_size = size;
 	ft_memcpy(new_ptr, ptr, old_size);
 	free(ptr);
