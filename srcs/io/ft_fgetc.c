88e650ec357f0f00c8acb8c4fc89815c37ea5b18
Riyane El Qoqui <riyane.dev@gmail.com>
2025-07-20 00:40:02 +0200

    feat(core/io): implement ft_fgetc, ft_getc, and ft_getchar functions for input handling
    Signed-off-by: Riyane El Qoqui <riyane.dev@gmail.com>

---
+++ srcs/io/ft_fgetc.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fgetc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 05:00:26 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/07/19 05:26:03 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "io/ft_io.h"
+
+static int	fill_buffer(t_ft_file *stream)
+{
+	ssize_t	bytes_read;
+
+	bytes_read = read(stream->fd, stream->buffer, stream->buffer_size);
+	if (bytes_read <= 0)
+	{
+		if (bytes_read == 0)
+			stream->eof = 1;
+		else
+			stream->error = 1;
+		return (-1);
+	}
+	stream->buffer_len = (size_t)bytes_read;
+	stream->buffer_pos = 0;
+	return (0);
+}
+
+int	ft_fgetc(t_ft_file *stream)
+{
+	unsigned char	c;
+
+	if (!stream || stream->error || stream->eof)
+		return (FT_EOF);
+	if (stream->buffer_pos >= stream->buffer_len)
+	{
+		if (fill_buffer(stream) == -1)
+			return (FT_EOF);
+	}
+	c = (unsigned char)stream->buffer[stream->buffer_pos];
+	stream->buffer_pos++;
+	return ((int)c);
+}
