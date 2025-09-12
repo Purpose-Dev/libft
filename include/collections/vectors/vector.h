7e4d62979630b48ae9274f9e95ea6c6c8e8ad4b4
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-12 11:03:07 +0200

    refactor(collections/vector): update type definitions and add const qualifiers to function prototypes

--- include/collections/vectors/vector.h
+++ include/collections/vectors/vector.h
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 03:25:15 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/12 03:31:51 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/09/12 11:01:18 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,8 @@ typedef union u_any
 	int64_t		i64;
 	uint32_t	u32;
 	int32_t		i32;
-	_Float32	f32;
-	_Float64	f64;
+	float		f32;
+	double		f64;
 }	t_any;
 
 typedef struct s_value
@@ -65,19 +65,19 @@ t_value		ft_gen_val(t_type type, t_any value);
 
 t_result	ft_vector_init(t_vector *vec, size_t size);
 t_result	ft_vector_resize(t_vector *vec, size_t size);
-void		ft_vector_free(t_vector *vec);
-void		ft_vector_foreach(t_vector *vec,
+void		ft_vector_free(const t_vector *vec);
+void		ft_vector_foreach(const t_vector *vec,
 				void (*fn)(size_t idx, t_value data));
-void		ft_vector_map(t_vector *vec,
+void		ft_vector_map(const t_vector *vec,
 				t_value (*fn)(size_t idx, t_value data));
-_Bool		ft_vector_every(t_vector *vec,
+_Bool		ft_vector_every(const t_vector *vec,
 				_Bool (*fn)(size_t idx, t_value data));
-_Bool		ft_vector_some(t_vector *vec,
+_Bool		ft_vector_some(const t_vector *vec,
 				_Bool (*fn)(size_t idx, t_value data));
 t_value		ft_vector_pop(t_vector *vec);
 t_value		ft_vector_shift(t_vector *vec);
 t_result	ft_vector_push(t_vector *vec, t_value value);
 t_result	ft_vector_unshift(t_vector *vec, t_value value);
-t_value		ft_vector_at(t_vector *vec, ssize_t idx);
+t_value		ft_vector_at(const t_vector *vec, ssize_t i);
 
 #endif // VECTOR_H
