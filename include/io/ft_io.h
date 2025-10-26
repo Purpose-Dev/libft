80103e2de8856cb81a520719b3002c1652edbc53
Riyane El Qoqui <riyane.dev@gmail.com>
2025-10-26 19:53:40 +0100

    feat(io): add ft_fwrite for writing data to custom streams

--- include/io/ft_io.h
+++ include/io/ft_io.h
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 00:56:45 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/11 19:28:56 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/10/26 19:44:42 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,8 @@ int			ft_getc(t_ft_file *stream);
 int			ft_getchar(void);
 char		*ft_fgets(char *str, int size, t_ft_file *stream);
 size_t		ft_fread(void *ptr, size_t size, size_t count, t_ft_file *stream);
+ssize_t		ft_fwrite(const void *ptr, size_t size, size_t count,
+				t_ft_file *stream);
 ssize_t		ft_getline(char **line_ptr, size_t *n, t_ft_file *stream);
 int			ft_feof(t_ft_file *stream);
 
---
+++ src/io/ft_fwrite.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fwrite.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/26 19:46:42 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/10/26 19:48:56 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "io/ft_io.h"
+
+ssize_t	ft_fwrite(const void *ptr, const size_t size, const size_t count,
+					t_ft_file *stream)
+{
+	const char	*data;
+	size_t		total_bytes;
+	ssize_t		written;
+
+	if (!ptr || !stream || size == 0 || count == 0)
+		return (0);
+	total_bytes = size * count;
+	data = (const char *)ptr;
+	written = write(stream->fd, data, total_bytes);
+	if (written < 0)
+	{
+		stream->error = 1;
+		return (-1);
+	}
+	return (written);
+}
