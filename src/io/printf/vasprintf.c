9ffccfedb832d70e97dc8675504b679088240bef
Riyane El Qoqui <riyane.dev@gmail.com>
2025-10-26 19:53:46 +0100

    feat(io): add ft_vasprintf for formatted output to strings

--- src/io/printf/vasprintf.c
+++ src/io/printf/vasprintf.c
@@ -1,32 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vfprintf.c                                         :+:      :+:    :+:   */
+/*   vasprintf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/26 19:40:20 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/10/26 19:51:31 by rel-qoqu         ###   ########.fr       */
+/*   Created: 2025/10/26 19:49:24 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/10/26 19:52:18 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "io/ft_printf.h"
 
-int	ft_vfprintf(t_ft_file *stream, const char *fmt, va_list ap)
+int	ft_vasprintf(char **strp, const char *fmt, va_list ap)
 {
-	char	*buffer;
+	va_list	ap_copy;
 	int		len;
-	ssize_t	written;
+	char	*buffer;
 
-	if (!stream || !fmt)
+	if (!strp || !fmt)
 		return (-1);
-	buffer = NULL;
-	len = ft_vasprintf(&buffer, fmt, ap);
-	if (len < 0 || !buffer)
+	va_copy(ap_copy, ap);
+	len = ft_vsnprintf(NULL, 0, fmt, ap_copy);
+	va_end(ap_copy);
+	if (len < 0)
 		return (-1);
-	written = ft_fwrite(buffer, 1, (size_t)len, stream);
-	free(buffer);
-	if (written < 0)
+	buffer = (char *)malloc((size_t)len +1);
+	if (!buffer)
 		return (-1);
-	return ((int)written);
+	len = ft_vsnprintf(buffer, (size_t)len + 1, fmt, ap);
+	if (len < 0)
+	{
+		free(buffer);
+		return (1);
+	}
+	*strp = buffer;
+	return (len);
 }
