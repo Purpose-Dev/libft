b87ff80e4af18a973ba81cd5319e30c7098c3045
Riyane El Qoqui <riyane.dev@gmail.com>
2025-11-07 09:15:25 +0100

    feat(log): extend logging system with thread-safe logger and support for multiple log sinks

--- include/core/log/log.h
+++ include/core/log/log.h
@@ -6,16 +6,21 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 08:53:41 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/11/07 09:05:40 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/11/07 09:14:13 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LOG_H
 # define LOG_H
 
+# include <pthread.h>
 # include <stdbool.h>
 
 # include "io/ft_io.h"
+
+# ifndef MAX_LOG_SINKS
+#  define MAX_LOG_SINKS 16
+# endif // MAX_LOG_SINKS
 
 typedef enum e_log_level
 {
@@ -31,7 +36,19 @@ typedef struct s_log_sink
 	t_ft_file	*stream;
 	t_log_level	min_level;
 	bool		use_color;
+	char		padding[3];
 }	t_log_sink;
+
+typedef struct s_logger
+{
+	t_log_sink		sinks[MAX_LOG_SINKS];
+	pthread_mutex_t	mutex;
+	t_log_level		global_min_level;
+	int				sink_count;
+	bool			is_quiet;
+	bool			use_color;
+	char			padding[6];
+}	t_logger;
 
 void	log_init(void);
 void	log_destroy(void);
